59267c67dbfe7ac3a020930d96dcc64c386b45df
Yasmine <yasmine.chenafa@gmail.com>
2024-04-26 21:20:59 +0100

    edit modification

--- models/product.py
+++ models/product.py
@@ -1,6 +1,19 @@
 from odoo import fields, models, api
 import logging as log
 
+
+class ProductTemplate(models.Model):
+
+    _inherit = 'product.template'
+
+    @api.model
+    def create(self, vals):
+        log.warning('create product temp ---------------')
+        log.warning(vals)
+        if not 'service_to_purchase' in vals:
+            vals['service_to_purchase'] = False
+        return super(ProductTemplate, self).create(vals)
+
 class ProductProduct(models.Model):
 
     _inherit = 'product.product'
@@ -11,14 +24,14 @@ class ProductProduct(models.Model):
         product = self.env['product.product']
         for dictt in data :
             #product_brand_id
-            if dictt['product_brand_id']:
+            if "product_brand_id" in dictt  and dictt['product_brand_id']:
                 brand = self.env['product.brand'].search([('id_exchange','=',dictt['product_brand_id'][0])])
                 if not brand :
                     brand = self.env['product.brand'].create({'id_exchange': dictt['product_brand_id'][0],'name': dictt['product_brand_id'][1],'logo': dictt['product_brand_id'][2]})
                 dictt['product_brand_id'] = brand.id
 
             #x_product_brand_id2
-            if dictt['x_product_brand_id2']:
+            if "x_product_brand_id2" in dictt  and dictt['x_product_brand_id2']:
                 brand2 = self.env['product.brand'].search([('id_exchange','=',dictt['x_product_brand_id2'][0])])
                 if not brand2 :
                     brand2 = self.env['product.brand'].create({'id_exchange': dictt['x_product_brand_id2'][0],'name': dictt['x_product_brand_id2'][1],'logo': dictt['x_product_brand_id2'][2]})
@@ -26,18 +39,18 @@ class ProductProduct(models.Model):
 
 
             #intrastat_code_id
-            if dictt['intrastat_code_id']:
+            if "intrastat_code_id" in dictt  and dictt['intrastat_code_id']:
                 intrastat = self.env['account.intrastat.code'].search([('code','=',dictt['intrastat_code_id'])])
                 dictt['intrastat_code_id'] = intrastat.id if intrastat else False
 
             #intrastat_origin_country_id
-            if dictt['intrastat_origin_country_id']:
+            if "intrastat_origin_country_id" in dictt  and dictt['intrastat_origin_country_id']:
                 country = self.env['res.country'].search([('code','=',dictt['intrastat_origin_country_id'])])
                 dictt['intrastat_origin_country_id'] = intrastat_origin_country_id.id if intrastat_origin_country_id else False
             
 
             #product_family_id
-            if dictt['product_family_id']:
+            if "product_family_id" in dictt  and dictt['product_family_id']:
                 family = self.env['product.family'].search([('id_exchange','=',dictt['product_family_id'][0])])
                 if not family :
                     family = self.env['product.family'].create({'id_exchange': dictt['product_family_id'][0],'name': dictt['product_family_id'][1]})
@@ -45,19 +58,34 @@ class ProductProduct(models.Model):
 
 
             #product_sub_family_id
-            if dictt['product_sub_family_id']:
+            if "product_sub_family_id" in dictt  and dictt['product_sub_family_id']:
                 subfamily = self.env['product.sub.family'].search([('id_exchange','=',dictt['product_sub_family_id'][0])])
                 if not subfamily :
                     subfamily = self.env['product.sub.family'].create({'id_exchange': dictt['product_sub_family_id'][0],'name': dictt['product_sub_family_id'][1]})
                 dictt['product_sub_family_id'] = subfamily.id
 
             #categ_id
-            if dictt['categ_id']:
+            if "categ_id" in dictt  and dictt['categ_id']:
                 categ = self.env['product.category'].search([('id_exchange','=',dictt['categ_id'])])
                 dictt['categ_id'] = categ.id
 
+            #multi_categ_id
+            if "multi_categ_id" in dictt  and dictt['multi_categ_id']:
+                categ = self.env['product.category'].search([('id_exchange','=',dictt['multi_categ_id'])])
+                dictt['multi_categ_id'] = categ.id
+
+            #category2_id
+            if "category2_id" in dictt  and dictt['category2_id']:
+                categ = self.env['product.category'].search([('id_exchange','=',dictt['category2_id'])])
+                dictt['category2_id'] = categ.id
+
+            #category3_id
+            if "category3_id" in dictt  and dictt['category3_id']:
+                categ = self.env['product.category'].search([('id_exchange','=',dictt['category3_id'])])
+                dictt['category3_id'] = categ.id
+
             #container_ids
-            if dictt['container_ids']:
+            if "container_ids" in dictt  and dictt['container_ids']:
                 container = self.env['container'].search([('name','=',dictt['container_ids'])])
                 if not container:
                     container = self.env['container'].create({'name': dictt['container_ids']})
@@ -65,7 +93,7 @@ class ProductProduct(models.Model):
                 dictt['container_ids'] = container.id
 
             #shape_ids
-            if dictt['shape_ids']:
+            if "shape_ids" in dictt  and dictt['shape_ids']:
                 shape = self.env['shape'].search([('name','=',dictt['shape_ids'])])
                 if not shape:
                     shape = self.env['shape'].create({'name': dictt['shape_ids']})
@@ -75,7 +103,7 @@ class ProductProduct(models.Model):
 
 
             #capacity_ids
-            if dictt['capacity_ids']:
+            if "capacity_ids" in dictt  and dictt['capacity_ids']:
                 capacity = self.env['capacity'].search([('name','in',dictt['capacity_ids'])])
 
                 missing_capacity_names = [capacity_name for capacity_name in dictt['capacity_ids'] if capacity_name not in set(capacity.mapped('name'))]
@@ -87,7 +115,7 @@ class ProductProduct(models.Model):
 
 
             #color_ids
-            if dictt['color_ids']:
+            if "color_ids" in dictt  and dictt['color_ids']:
                 color = self.env['color'].search([('name','in',dictt['color_ids'])])
 
                 missing_color_names = [color_name for color_name in dictt['color_ids'] if color_name not in set(color.mapped('name'))]
@@ -98,7 +126,7 @@ class ProductProduct(models.Model):
 
             
             #for_who_ids
-            if dictt['for_who_ids']:
+            if "for_who_ids" in dictt  and dictt['for_who_ids']:
                 for_who = self.env['for_who'].search([('name','in',dictt['for_who_ids'])])
 
                 missing_for_who_names = [for_who_name for for_who_name in dictt['for_who_ids'] if for_who_name not in set(for_who.mapped('name'))]
@@ -110,11 +138,22 @@ class ProductProduct(models.Model):
             
 
             #taxes_id
-            if dictt['taxes_id']:
+            if "taxes_id" in dictt  and dictt['taxes_id']:
                 taxes = self.env['account.tax'].search([('name','in',dictt['taxes_id'])])
    
-                   
                 dictt['taxes_id'] = [(6,0,taxes.ids)] if taxes else False
+
+            #supplier_taxes_id
+            if "supplier_taxes_id" in dictt  and dictt['supplier_taxes_id']:
+                taxes = self.env['account.tax'].search([('name','in',dictt['supplier_taxes_id'])])
+   
+                dictt['supplier_taxes_id'] = [(6,0,taxes.ids)] if taxes else False
+
+            if "multi_barcode_ids" in vals and vals['multi_barcode_ids']:
+                barcodes = self.env['product.multiple.barcodes']
+                for val in vals['multi_barcode_ids']:
+                    barcodes += self.env['product.multiple.barcodes'].create({'name':val})
+                dictt['multi_barcode_ids'] = [(6,0,barcodes.ids)] if barcodes else False
 
             keys_to_delete = [key for key, value in dictt.items() if value is False]
             for key in keys_to_delete:
